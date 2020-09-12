8d87cae501ee22efda0ef08c4e21ea219880d93c
Boris Drazhzhov <templar8@gmail.com>
2020-09-12 13:51:05 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+It's a basic cmake project allowing to start developing of QT application.
+
+I use it with [vcpkg](https://github.com/microsoft/vcpkg).
+But I think it will work without it.
---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+﻿#include <QApplication>
+#include <QPushButton>
+
+int main(int argc, char *argv[])
+{
+	QApplication a(argc, argv);
+
+    QPushButton button ("Hello world!");
+    button.show();
+
+    return a.exec();
+}
